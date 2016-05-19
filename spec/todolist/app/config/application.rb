83cb472aa76b0cc0234187bd6f998798253bb7ec
Femi Senjobi <femi.senjobi@andela.com>
2016-05-19 16:35:51 +0100

    change location of application file

--- spec/todolist/app/config/application.rb
+++
@@ -1,7 +0,0 @@
-require 'rollerskates'
-$LOAD_PATH << File.join(File.dirname(__FILE__), '..', '..', 'app', 'controllers')
-
-module Todolist
-  class Application < Rollerskates::Application
-  end
-end
