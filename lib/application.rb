ff970b5403c0c8f292c0332b0753255c670e8081
Femi Senjobi <femi.senjobi@andela.com>
2016-05-19 16:33:35 +0100

    add application file

---
+++ lib/application.rb
@@ -0,0 +1,23 @@
+module Rollerskates
+  class Application
+    attr_reader :routes
+
+    def initialize
+      @routes = Routing::Router.new
+    end
+
+    def call(env)
+      @request = Rack::Request.new(env)
+      route = mapper.map_to_route(@request)
+      if route
+        response = route.dispatch
+        return [200, { "Content-Type" => "text/html" }, [response]]
+      end
+      [404, {}, ["Route not found"]]
+    end
+
+    def mapper
+      @mapper ||= Routing::Mapper.new(routes.endpoints)
+    end
+  end
+end
