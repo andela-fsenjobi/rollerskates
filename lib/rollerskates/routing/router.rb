dce0bc26973b03f62104922f3b8b0609f915dd1a
Femi Senjobi <femi.senjobi@andela.com>
2016-06-02 16:53:22 +0100

    feat(router): implement only and except for resources
    [#120428211]

--- lib/rollerskates/routing/router.rb
+++ lib/rollerskates/routing/router.rb
@@ -5,7 +5,7 @@ module Rollerskates
         instance_eval &block
       end
 
-      create_routes = Proc.new do |method_name|
+      [:get, :post, :put, :patch, :delete].each do |method_name|
         define_method(method_name) do |path, to:|
           path = "/#{path}" unless path[0] = '/'
           klass_and_method = controller_and_action_for(to)
@@ -17,25 +17,40 @@ module Rollerskates
         end
       end
 
-      [:get, :post, :put, :patch, :delete].each(&create_routes)
-
       def default_actions
     		[
-    			["index", "get"],
-    			["create", "post"],
-    			["show", "get", ":id"],
-    			["destroy", "delete", ":id"],
-    			["update", "put", ":id"],
-          ["update", "patch", ":id"]
+    			[:index, :get],
+    			[:create, :post],
+    			[:show, :get, :id],
+          [:edit, :get, :id],
+          [:new, :get],
+    			[:destroy, :delete, :id],
+    			[:update, :put, :id],
+          [:update, :patch, :id]
     		]
     	end
 
+      def resources_only(*options)
+        default_actions.select do |arr|
+          options.include? arr[0]
+        end
+      end
+
+      def resources_except(*options)
+        default_actions.reject do |arr|
+          options.include? arr[0]
+        end
+      end
+
     	def resources(controller_name, options = {})
-    	  default_actions.each do |value|
-    	  	  path_suffix = value[2] ? "/#{value[2]}" : ""
-    	  	  path = "/#{controller_name}#{path_suffix}"
-            action = "#{controller_name}##{value[0]}"
-            send(value[1], path, to: action)
+        actions = default_actions
+        actions = resources_only(options[:only]) if options[:only]
+        actions = resources_except(options[:except]) if options[:except]
+    	  actions.each do |value|
+  	  	  path_suffix = value[2] ? "/#{value[2]}" : ""
+  	  	  path = "/#{controller_name}#{path_suffix}"
+          action = "#{controller_name}##{value[0]}"
+          send(value[1], path, to: action)
     	  end
     	end
 
