0f55a001a8ab981349f79cc24a0cd1ed42dd94fd
Femi Senjobi <femi.senjobi@andela.com>
2016-06-02 15:49:26 +0100

    feat(base controller): render instance variables on view
    [Starts #120428211]

--- lib/rollerskates/base_controller.rb
+++ lib/rollerskates/base_controller.rb
@@ -1,16 +1,46 @@
 require "erb"
 require "tilt"
+require "pry"
 
 module Rollerskates
+  class ViewObject
+    attr_reader :main_object
+
+    def initialize(obj)
+      @main_object = obj
+    end
+    # 
+    # def method_missing(method, *args)
+    #   @main_object.send(method, *args)
+    # end
+  end
+
   class BaseController
     def initialize(env)
       @request ||= env
     end
 
+    def get_instance_variables
+      hash = {}
+      variables = instance_variables
+      variables -= [:@request]
+      variables.each { |var| hash[var[1..-1].to_sym] = instance_variable_get(var) }
+      hash
+    end
+
     def render(view_name, locals={})
       file_name = File.join("app", "views", controller_name, "#{view_name}.erb")
       template = Tilt.new(file_name)
-      template.render(locals)
+      obj = get_view_object
+      template.render obj, locals
+    end
+
+    def get_view_object
+      obj = ViewObject.new(self)
+      get_instance_variables.each do |key, value|
+        obj.instance_variable_set("@#{key}", value)
+      end
+      obj
     end
 
     def controller_name
