a6d78309f046cd99b9088ea90f888ac5946f17df
Femi Senjobi <femi.senjobi@andela.com>
2016-06-10 09:12:11 +0100

    chore(base controller): remove redundant methods
    remove view object since instance variables exist in self
    remove get instance variable class
    
    [#120428211]

--- lib/rollerskates/base_controller.rb
+++ lib/rollerskates/base_controller.rb
@@ -3,33 +3,22 @@ require "tilt"
 require "pry"
 
 module Rollerskates
-  class ViewObject
-    attr_reader :main_object
-
-    def initialize(obj)
-      @main_object = obj
-    end
-    #
-    # def method_missing(method, *args)
-    #   @main_object.send(method, *args)
-    # end
-  end
 
   class BaseController
     def initialize(env)
       @request ||= env
     end
 
-    def get_instance_variables
-      hash = {}
-      variables = instance_variables
-      variables -= [:@request]
-      variables.each { |var| hash[var[1..-1].to_sym] = instance_variable_get(var) }
-      hash
+    def response(body = [], status = 200, header = { "Content-type" => "text/html" })
+      @response = Rack::Response.new(body, status, header)
     end
 
-    def response(body = [], status = 200, header = {})
-      @response = Rack::Response.new(body, status, header)
+    def redirect_to(path, status: 301)
+      response([], status, { "Location" => path })
+    end
+
+    def params
+      @request.params
     end
 
     def get_response
@@ -43,19 +32,10 @@ module Rollerskates
     def render_template(view_name, locals={})
       file_name = File.join("app", "views", controller_name, "#{view_name}.erb")
       template = Tilt.new(file_name)
-      obj = get_view_object
-      template.render obj, locals
+      template.render self, locals
     end
 
-    def get_view_object
-      obj = ViewObject.new(self)
-      get_instance_variables.each do |key, value|
-        obj.instance_variable_set("@#{key}", value)
-      end
-      obj
-    end
-
-    def finish(method_name)
+    def finish(method_name, status = nil)
       if get_response
         get_response
       else
