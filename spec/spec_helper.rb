99b8e3c111eab5ce4a4dc1cb539809872c4e0550
Femi Senjobi <femi.senjobi@andela.com>
2016-06-14 17:24:03 +0100

    test(unit): update unit tests
    [#120829713]

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,15 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'todolist/config/application.rb'
-require 'rspec'
-require 'rack/test'
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+require "todolist/config/application"
+require "rspec"
+require "capybara"
 
-ENV['RACK_ENV'] = 'test'
+RSpec.shared_context type: :feature do
+  require "capybara/rspec"
+  before(:all) do
+    app = Rack::Builder.
+          parse_file(File.join(__dir__, "todolist", "config.ru")).first
+    Capybara.app = app
+  end
+end
+
+ENV["RACK_ENV"] = "test"
