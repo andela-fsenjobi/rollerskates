0f5d297ac0a1b9e7fdaabdd89ff5029b9f72ee59
Femi Senjobi <femi.senjobi@andela.com>
2016-06-15 12:55:21 +0100

    test(rollerskates): include tests
    add coveralls integration
    add tests for base model
    add integration tests for todo app
    update tests for router
    add db folder for tests
    
    [#121510309]

--- spec/rollerskates_spec.rb
+++ spec/rollerskates_spec.rb
@@ -1,69 +1,7 @@
-require 'spec_helper'
-
-describe 'Todolist App' do
-  include Rack::Test::Methods
-
-  TodoApplication = Todolist::Application.new
-  def app
-    require "todolist/config/routes.rb"
-    TodoApplication
-  end
-  #
-  # it 'returns about page' do
-  #   get "/todolist/about"
-  #   expect(last_response).to be_ok
-  #   expect(last_response.body).to eq("Hey Femi")
-  # end
-
-  it 'returns a list of all my todos' do
-    get '/todolist'
-    expect(last_response).to be_ok
-    expect(last_response.body).to eq(
-      "['Write a book', 'Build a house', 'Get married', 'Buy a car']"
-    )
-  end
-
-  it 'returns first item in my todolist' do
-    get '/todolist/1'
-    expect(last_response).to be_ok
-    expect(last_response.body).to eq('Write a book')
-  end
-
-  it 'can respond to post request' do
-    post '/todolist'
-    expect(last_response).to be_ok
-    expect(last_response.body).to eq('Post go swimming')
-  end
-
-  it 'returns a list of all my todos' do
-    get '/todolist'
-    expect(last_response).to be_ok
-    expect(last_response.body).to eq(
-      "['Write a book', 'Build a house', 'Get married', 'Buy a car']"
-    )
-  end
-
-  it 'can respond to put request' do
-    put '/todolist/10'
-    expect(last_response).to be_ok
-    expect(last_response.body).to eq('Put Write a book')
-  end
-
-  it 'can respond to get request' do
-    get '/todolist/10/edit'
-    expect(last_response).to be_ok
-    expect(last_response.body).to eq('Put Write a book')
-  end
-
-  it 'can respond to delete request' do
-    delete '/todolist/12'
-    expect(last_response).to be_ok
-    expect(last_response.body).to eq('Delete Write a book')
-  end
-end
+require "spec_helper"
 
 describe Rollerskates do
-  it 'has a version number' do
+  it "has a version number" do
     expect(Rollerskates::VERSION).not_to be nil
   end
 end
